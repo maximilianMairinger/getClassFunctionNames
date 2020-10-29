9795ce41be5f8a48041b2f304651c6e7d24df9a3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-10-30 00:24:08 +0100

    implemented functionality

--- app/src/getClassFunctionNames.ts
+++ app/src/getClassFunctionNames.ts
@@ -1,5 +1,22 @@
 
 
-export default function() {
+const constructorString: "constructor" = "constructor"
+
+export default function(Class: any, UpToBaseClass: any = Object, removeFunctionNames: string[] = []) {
+  let functionNameList: string[] = []
+  let cur = Class
+
+  if (!removeFunctionNames.includes(constructorString)) removeFunctionNames.push(constructorString)
   
+  while (cur.prototype instanceof UpToBaseClass) {
+    Object.getOwnPropertyNames(cur.prototype).forEach((e) => {
+      if (!removeFunctionNames.includes(e)) {
+        functionNameList.push(e)
+        removeFunctionNames.push(e)
+      }
+    })
+    cur = Object.getPrototypeOf(cur)
+  }
+
+  return functionNameList
 }
