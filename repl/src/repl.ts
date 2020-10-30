e2dfb2b782f1ca59fc21fc38798e0928a6365383
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-10-30 13:44:42 +0100

    testing

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,4 +1,13 @@
 import getClassFunctionNames from "./../../app/src/getClassFunctionNames"
 //const testElem = document.querySelector("#test")
 
-getClassFunctionNames()
+class AAA extends Array {
+  qq() {
+
+  }
+  ww() {
+
+  }
+}
+
+console.log(getClassFunctionNames(AAA, Array))
